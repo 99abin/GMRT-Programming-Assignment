e2c18a61cff436811104b0d29ed380f106937fcc
Abin <bintanghidayatullah021@gmail.com>
2025-02-22 08:17:17 +0700

    added comments and simplified code

--- usncandled.cpp
+++ usncandled.cpp
@@ -1,53 +1,43 @@
-#include <NewPing.h>
-#include <Servo.h>
+#include <NewPing.h> // Include the NewPing library for ultrasonic distance measurement
+#include <Servo.h>   // Include the Servo library for controlling servo motors
 
-const int ledM = 13;
-const int ledB = 12;
-const int ledH = 11;
-Servo mesin;
-NewPing sensor(4, 2, 400);
-int jarak;
+const int ledPins[] = {13, 12, 11}; // Array for LED pins (M, B, H)
+Servo mesin; // Create a Servo object to control the servo motor
+NewPing sensor(4, 2, 400); // Create a NewPing object with trigger pin 4, echo pin 2, and max distance of 400 cm
 
 void setup() {
-  mesin.attach(8);
-  pinMode(ledM, OUTPUT);
-  pinMode(ledB, OUTPUT);
-  pinMode(ledH, OUTPUT);
+  mesin.attach(8); // Attach the servo motor to pin 8
+  for (int i = 0; i < 3; i++) {
+    pinMode(ledPins[i], OUTPUT); // Set each LED pin as an output
+  }
 }
 
 void loop() {
-  jarak = sensor.ping_cm();
-  
+  int jarak = sensor.ping_cm(); // Measure the distance in centimeters using the ultrasonic sensor
+  int ledState = -1; // Variable to determine which LED to light up
+  int servoPosition = 0; // Variable to store the servo position
+
+  // Determine which LED to light up and the servo position based on the distance
   if (jarak % 2 == 0 && jarak % 3 != 0 && jarak % 7 != 0) {
-    digitalWrite(ledM, HIGH);
-    digitalWrite(ledB, LOW);
-    digitalWrite(ledH, LOW);
-    mesin.write(20);
+    ledState = 0; // Light up the first LED
+    servoPosition = 20; // Set servo position to 20 degrees
+  } else if (jarak % 3 == 0 && jarak % 2 != 0 && jarak % 7 != 0) {
+    ledState = 1; // Light up the second LED
+    servoPosition = 30; // Set servo position to 30 degrees
+  } else if (jarak % 7 == 0 && jarak % 3 != 0 && jarak % 2 != 0) {
+    ledState = 2; // Light up the third LED
+    servoPosition = 70; // Set servo position to 70 degrees
+  } else if (jarak % 2 == 0 && jarak % 3 == 0 && jarak % 7 != 0) {
+    ledState = 0; // Light up the first LED
+    digitalWrite(ledPins[1], HIGH); // Light up the second LED
+    servoPosition = 50; // Set servo position to 50 degrees
   }
-  else if (jarak % 3 == 0 && jarak % 2 != 0 && jarak % 7 != 0) {
-    digitalWrite(ledM, LOW);
-    digitalWrite(ledB, HIGH);
-    digitalWrite(ledH, LOW);
-    mesin.write(30);
+
+  // Turn off all LEDs if none of the conditions are met
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(ledPins[i], (i == ledState) ? HIGH : LOW); // Light up the selected LED
   }
-  else if (jarak % 7 == 0 && jarak % 3 != 0 && jarak % 2 != 0) {
-    digitalWrite(ledM, LOW);
-    digitalWrite(ledB, LOW);
-    digitalWrite(ledH, HIGH);
-    mesin.write(70);
-  }
-  else if (jarak % 2 == 0 && jarak % 3 == 0 && jarak % 7 != 0) {
-    digitalWrite(ledM, HIGH);
-    digitalWrite(ledB, HIGH);
-    digitalWrite(ledH, LOW);
-    mesin.write(50);
-  }
-  else if (jarak % 2 != 0 && jarak % 3 != 0 && jarak % 7 != 0) {
-    digitalWrite(ledM, LOW);
-    digitalWrite(ledB, LOW);
-    digitalWrite(ledH, LOW);
-    mesin.write(0);
-  }
-  
-  delay(500);
+
+  mesin.write(servoPosition); // Set the servo position
+  delay(500); // Wait for 500 milliseconds before the next loop iteration
 }
